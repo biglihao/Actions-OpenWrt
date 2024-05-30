7a5675c6a46cee1fe8b9324c5e2ca121914bade7
biglihao <biglihao@gmail.com>
2024-05-30 11:28:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/172.16.0.1/g' package/base-files/files/bin/config_generate
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 修改 argon 为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 # 删除原默认主题
 rm -rf package/lean/luci-theme-bootstrap
